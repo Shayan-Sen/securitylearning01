0f4a4d466ab1a401ae9eb4cdbf20295c7a51bcec
Shayan Sen <senshayan2103@gmail.com>
2025-03-26 09:22:05 +0530

    init

---
+++ src/main/java/com/example/shayan/securitylearning01/Securitylearning01Application.java
@@ -0,0 +1,13 @@
+package com.example.shayan.securitylearning01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securitylearning01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Securitylearning01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shayan/securitylearning01/Securitylearning01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shayan.securitylearning01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securitylearning01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
