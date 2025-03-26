0721bd6af5e0f900adcbc7270479aef9fda72ee4
Shayan Sen <senshayan2103@gmail.com>
2025-03-26 17:20:40 +0530

    meh

---
+++ src/main/java/com/example/shayan/securitylearning01/repository/Todo.java
@@ -0,0 +1,32 @@
+package com.example.shayan.securitylearning01.repository;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "todos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Todo {
+    
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.RANDOM)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String title;
+
+    private String content;
+}
--- src/main/java/com/example/shayan/securitylearning01/repository/User.java
+++ src/main/java/com/example/shayan/securitylearning01/repository/User.java
@@ -1,16 +1,18 @@
 package com.example.shayan.securitylearning01.repository;
 
-import java.util.UUID;
-
 import org.hibernate.annotations.UuidGenerator;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.CascadeType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -18,15 +20,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    
+
     @Id
     @UuidGenerator(style = UuidGenerator.Style.RANDOM)
     @Column(name = "id", nullable = false, unique = true)
-    private UUID id;
+    private String id;
 
-    @Column(unique = true,nullable = false,name = "username")
+    @Column(unique = true, nullable = false, name = "username")
     private String username;
-    
-    @Column(nullable = false,name = "password")
+
+    @Column(nullable = false, name = "password")
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = { CascadeType.REMOVE }, orphanRemoval = true)
+    private List<Todo> todos;
 }
